f8825e76f82374bb6c9ed741068b211e4b2f9da2
Dennis Kaselow <dennis.kaselow+github@gmail.com>
2016-08-27 19:20:33 +0200

    changed curve behaviour

--- lib/src/client/systems/events.dart
+++ lib/src/client/systems/events.dart
@@ -19,17 +19,19 @@ class InputHandlingSystem extends GenericInputHandlingSystem {
     if (up) {
       a.value = a.maxAcceleration;
       b.value = 1.0;
-      if (left) {
-        o.angle -= o.turnRate;
-      } else if (right) {
-        o.angle += o.turnRate;
-      }
     } else if (down) {
       b.value = b.brakeForce;
       a.value = 0.0;
     } else {
       b.value = 1.0;
       a.value = 0.0;
+    }
+    if (left) {
+      o.angle -= o.turnRate;
+      a.value = a.maxAcceleration;
+    } else if (right) {
+      o.angle += o.turnRate;
+      a.value = a.maxAcceleration;
     }
   }
 }
